8df535a5a80306e623038e00a074b064840a4381
Priyanka <primo8296@gmail.com>
2023-02-15 16:29:59 +0530

    splashscreen

--- app/src/main/java/com/app/ktorcrud/activities/SplashActivity.kt
+++ app/src/main/java/com/app/ktorcrud/activities/SplashActivity.kt
@@ -3,18 +3,40 @@ package com.app.ktorcrud.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.app.ktorcrud.R
 import com.app.ktorcrud.utils.Utils
+import kotlinx.coroutines.launch
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
+        setupSplashScreen()
         super.onCreate(savedInstanceState)
-        splashScreen.setKeepOnScreenCondition { true }
-        if (sharedPreferenceUtils.preferenceGetBoolean(Utils.PreferenceKey.isLoggedIn, false)) {
-            startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
-        } else {
-            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+        Timer().schedule(500){
+            if (sharedPreferenceUtils.preferenceGetBoolean(Utils.PreferenceKey.isLoggedIn, false)) {
+                startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
+            finish()
         }
-        finish()
+
+    }
+
+    private fun setupSplashScreen() {
+        var keepSplashScreenOn = true
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                keepSplashScreenOn=false
+            }
+        }
+
+        installSplashScreen().setKeepOnScreenCondition {
+            keepSplashScreenOn
+        }
     }
 }
